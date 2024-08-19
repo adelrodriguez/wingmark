7d518c0fc2690be59a4519d62db8f0c032a61b05
adelrodriguez <github-action@actions-template-sync.noreply.github.com>
2024-08-19 16:49:07 +0000

    chore(template): merge template changes :up:
    Signed-off-by: adelrodriguez <github-action@actions-template-sync.noreply.github.com>

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,11 +1,3 @@
-export class BrowserError extends Error {
-  message = "Unable to start browser instance"
-}
-
-export class ReadabilityError extends Error {
-  message = "Unable to parse article content"
-}
-
 export class InternalError extends Error {}
 
 /**
